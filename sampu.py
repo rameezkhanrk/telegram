664271fadcb30f8bb5b04138d32ed1e951f496a2
rameezkhanrk <88378836+rameezkhanrk@users.noreply.github.com>
2022-01-14 17:07:13 +0530

    Create sampu.py

---
+++ sampu.py
@@ -0,0 +1,13 @@
+import asyncio
+#Credit goes to @RameezKhan
+@ultroid_cmd(pattern="sampu")
+async def naruto(ult):
+	await ult.edit("Hello")
+	await asyncio.sleep(1)
+	await ult.edit("Im Sampad Eda")
+	await asyncio.sleep(3)
+	await ult.edit("ig u know me")
+	await asyncio.sleep(2)
+	await ult.edit("i m pepe spammer")
+	await asyncio.sleep(1)
+	await ult.edit("@MEGABLAZIKEN")
